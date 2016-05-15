4d123c1dd66325e98fefcd24fa70299b26703d7c
Benjamin Young <byoungm@gmail.com>
2016-05-14 19:20:09 -0700

    Paritcle: added initial particle code
    Added inital incomplete particle code
    
    Signed-off-by: Benjamin Young <byoungm@gmail.com>

---
+++ Particle/PinControl.cpp
@@ -0,0 +1,9 @@
+#include "PinControl.h"
+
+// Static Members
+int PinControl::m_Leds[2];
+
+void PinControl::init()
+{
+    
+}
---
+++ Particle/PinControl.h
@@ -0,0 +1,10 @@
+
+class PinControl
+{
+public:
+    static void init();
+    
+private:
+    static int m_Leds[2];
+
+};
